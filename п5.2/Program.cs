fa282aab35f2201b38aaf1e347903ae178ef2049
HP <HP@WIN-8VMT11N5JJN>
2022-10-06 19:32:04 +0300

    Добавьте файлы проекта.

---
+++ п5.2/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 4, b = 3; // ввод целочисленных переменных a,b
+        double c = Math.Sqrt(a * a + b * b); // математическая формула для вычисления гипотенузы
+        Console.WriteLine("Гипотенуза = " + c + ""); // вывод на экран строки "..."
+    }
+}
